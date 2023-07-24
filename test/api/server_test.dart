260d5714f405eadf566893cad79a7cd0939c1d79
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2023-07-24 22:55:58 +0200

    Add-api test for nextGames

--- test/api/server_test.dart
+++ test/api/server_test.dart
@@ -18,13 +18,66 @@ void main() {
     );
 
     saveUser('test@unit.com', 'unit_test_token');
-    // Wait for server to start and print to stdout.
+
     await p.stdout.first;
   });
 
   tearDown(() async {
     p.kill();
   });
+
+  int getScore(List<dynamic> data) {
+    int score = 0;
+
+    for (var info in data) {
+      score += int.tryParse(info['homeScore'] ?? '') ?? 0;
+      score += int.tryParse(info['awayScore'] ?? '') ?? 0;
+    }
+
+    return score;
+  }
+
+  void checkTeam(List<dynamic> data, String expectedTeam) {
+    List<String> teams = [];
+
+    for (var info in data) {
+      teams.add(info['homeTeam']);
+      teams.add(info['awayTeam']);
+    }
+
+    Set<String> uniqueTeams = Set.from(teams);
+    bool findExpectedTeam = false;
+    for (var team in uniqueTeams) {
+      if (team.contains(expectedTeam)) {
+        findExpectedTeam = true;
+        break;
+      }
+    }
+    expect(findExpectedTeam, true, reason: '$expectedTeam is not in teams array');
+  }
+
+  void checkTime(List<dynamic> data) {
+    for (var info in data) {
+      expect(info.containsKey('time'), true);
+      String time = info['time'];
+
+      expect(time.length, 5);
+
+      List<String> timeInfo = time.split(':');
+      expect(timeInfo.length, 2);
+
+      int hour = int.tryParse(timeInfo[0]) ?? -1;
+      int minute = int.tryParse(timeInfo[1]) ?? -1;
+
+      expect(hour != -1, isTrue);
+      expect(minute != -1, isTrue);
+
+      // Additional check for the time range (0 <= hour <= 23, 0 <= minute <= 59)
+      expect(hour >= 0 && hour <= 23, true);
+      expect(minute >= 0 && minute <= 59, true);
+    }
+  }
+
 
   test('error: call url without x-auth-header', () async {
     final response = await get(Uri.parse('$host/api/club/next_games/id'));
@@ -67,6 +120,35 @@ void main() {
     expect(jsonObject['data'], isEmpty);
     expect(jsonObject['message'], equals('Exception: Error on URL: https://fussball.de/ajax.club.next.games/-/id/id/mode/PAGE'));
   });
+
+  test('success: api next game', () async {
+
+    final headers = {'x-auth-token': 'unit_test_token'};
+    final response = await get(Uri.parse('$host/api/club/next_games/00ES8GN91400002IVV0AG08LVUPGND5I'), headers: headers);
+    expect(response.statusCode, 200);
+
+
+    String decodedJson = utf8.decode(response.body.codeUnits);
+
+    Map<String, dynamic> jsonObject = json.decode(decodedJson);
+
+    expect(jsonObject, isNotNull);
+    expect(jsonObject['success'], isTrue);
+    expect(jsonObject['data'], isList);
+    expect(jsonObject['data'], isNotEmpty);
+
+    checkTime(jsonObject['data']);
+    checkTeam(jsonObject['data'], 'Fühlingen I');
+
+    int score = getScore(jsonObject['data']);
+    expect(score, 0);
+
+  });
+
+
+
+
+
   //
   // test('Echo', () async {
   //   final response = await get(Uri.parse('$host/echo/hello'));
