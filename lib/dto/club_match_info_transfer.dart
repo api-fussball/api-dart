09eb4da551fa8b91465708042fbad1e4f624f21f
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2023-07-31 21:22:08 +0200

    refactoring

---
+++ lib/dto/club_match_info_transfer.dart
@@ -0,0 +1,29 @@
+class ClubMatchInfoTransfer {
+  String homeScore = '';
+  String homeLogo = '';
+  String awayScore = '';
+  String awayLogo = '';
+  String time = '';
+  String date = '';
+  String homeTeam = '';
+  String awayTeam = '';
+  String ageGroup = '';
+  String competition = '';
+  String status = '';
+
+  Map<String, dynamic> toJson() {
+    return {
+      'homeScore': homeScore,
+      'homeLogo': homeLogo,
+      'awayScore': awayScore,
+      'awayLogo': awayLogo,
+      'time': time,
+      'date': date,
+      'homeTeam': homeTeam,
+      'awayTeam': awayTeam,
+      'ageGroup': ageGroup,
+      'competition': competition,
+      'status': status,
+    };
+  }
+}
--- lib/html/games.dart
+++ lib/html/games.dart
@@ -1,101 +1,77 @@
+import 'package:api_fussball_dart/dto/club_match_info_transfer.dart';
 import 'package:html/parser.dart' show parse;
 import 'package:html/dom.dart';
 
-
-class ClubMatchInfoTransfer {
-  String homeScore = '';
-  String homeLogo = '';
-  String awayScore = '';
-  String awayLogo = '';
-  String time = '';
-  String date = '';
-  String homeTeam = '';
-  String awayTeam = '';
-  String ageGroup = '';
-  String competition = '';
-  String status = '';
-
-  Map<String, dynamic> toJson() {
-    return {
-      'homeScore': homeScore,
-      'homeLogo': homeLogo,
-      'awayScore': awayScore,
-      'awayLogo': awayLogo,
-      'time': time,
-      'date': date,
-      'homeTeam': homeTeam,
-      'awayTeam': awayTeam,
-      'ageGroup': ageGroup,
-      'competition': competition,
-      'status': status,
-    };
-  }
+abstract interface class GamesInterface {
+  List<ClubMatchInfoTransfer> parseHTML(String html);
 }
 
+class Games implements GamesInterface {
+  @override
+  List<ClubMatchInfoTransfer> parseHTML(String html) {
+    List<ClubMatchInfoTransfer> results = [];
 
-List<ClubMatchInfoTransfer> parseHTML(String html) {
-  List<ClubMatchInfoTransfer> results = [];
+    var document = parse(html);
+    List<Element> rows = document.querySelectorAll('tr.visible-small');
 
-  var document = parse(html);
-  List<Element> rows = document.querySelectorAll('tr.visible-small');
+    for (var row in rows) {
+      ClubMatchInfoTransfer clubMatchInfoTransfer = ClubMatchInfoTransfer();
+      Element column = row.querySelectorAll('td')[0];
 
-  for (var row in rows) {
-    ClubMatchInfoTransfer clubMatchInfoTransfer = ClubMatchInfoTransfer();
-    Element column = row.querySelectorAll('td')[0];
+      _addTimeAndCompentionInfo(column.text, clubMatchInfoTransfer);
+      results.add(clubMatchInfoTransfer);
+    }
 
-    _addTimeAndCompentionInfo(column.text, clubMatchInfoTransfer);
-    results.add(clubMatchInfoTransfer);
-  }
+    List<Element> rowsScore = document.querySelectorAll('td.column-score');
 
-  List<Element> rowsScore = document.querySelectorAll('td.column-score');
+    for (var rowScore in rowsScore) {
+      ClubMatchInfoTransfer clubMatchInfoTransfer =
+          results[rowsScore.indexOf(rowScore)];
 
-  for (var rowScore in rowsScore) {
-    ClubMatchInfoTransfer clubMatchInfoTransfer = results[rowsScore.indexOf(rowScore)];
+      var awayTeam =
+          rowScore.previousElementSibling?.getElementsByTagName('span')[0];
 
-    var awayTeam = rowScore.previousElementSibling?.getElementsByTagName('span')[0];
+      if (awayTeam != null) {
+        clubMatchInfoTransfer.awayTeam = awayTeam.attributes['data-alt'] ?? '';
 
-    if(awayTeam != null) {
-      clubMatchInfoTransfer.awayTeam = awayTeam.attributes['data-alt'] ?? '';
+        var image = awayTeam.attributes['data-responsive-image'];
+        if (image != null) {
+          clubMatchInfoTransfer.awayLogo = 'https:$image';
+        }
+      }
 
-      var image = awayTeam.attributes['data-responsive-image'];
-      if(image != null) {
-        clubMatchInfoTransfer.awayLogo = 'https:$image';
+      var homeTeam = rowScore.previousElementSibling?.previousElementSibling
+          ?.previousElementSibling
+          ?.getElementsByTagName('span')[0];
+
+      if (homeTeam != null) {
+        clubMatchInfoTransfer.homeTeam = homeTeam.attributes['data-alt'] ?? '';
+
+        var image = homeTeam.attributes['data-responsive-image'];
+        if (image != null) {
+          clubMatchInfoTransfer.homeLogo = 'https:$image';
+        }
       }
     }
 
-    var homeTeam = rowScore.previousElementSibling?.previousElementSibling?.previousElementSibling?.getElementsByTagName('span')[0];
+    return results;
+  }
 
-    if(homeTeam != null) {
-      clubMatchInfoTransfer.homeTeam = homeTeam.attributes['data-alt'] ?? '';
+  void _addTimeAndCompentionInfo(
+      String text, ClubMatchInfoTransfer clubMatchInfoTransfer) {
+    String nodeValue = text.trim();
 
-      var image = homeTeam.attributes['data-responsive-image'];
-      if(image != null) {
-        clubMatchInfoTransfer.homeLogo = 'https:$image';
+    List<String> dateTimeInfo = nodeValue.split(' - ');
+    if (dateTimeInfo.isNotEmpty) {
+      clubMatchInfoTransfer.date =
+          dateTimeInfo[0].substring(dateTimeInfo[0].indexOf(',') + 2);
+      clubMatchInfoTransfer.time = dateTimeInfo[1].split(' Uhr')[0];
+
+      List<String> otherInfo = dateTimeInfo[1].split(' | ');
+      if (otherInfo.isNotEmpty) {
+        clubMatchInfoTransfer.ageGroup = otherInfo[1].trim();
+        clubMatchInfoTransfer.competition = otherInfo[2].trim();
       }
     }
-
-  }
-
-  return results;
-}
-
-
-void _addTimeAndCompentionInfo(String text,
-    ClubMatchInfoTransfer clubMatchInfoTransfer) {
-  String nodeValue = text.trim();
-
-  List<String> dateTimeInfo = nodeValue.split(' - ');
-  if (dateTimeInfo.isNotEmpty) {
-    clubMatchInfoTransfer.date =
-        dateTimeInfo[0].substring(dateTimeInfo[0].indexOf(',') + 2);
-    clubMatchInfoTransfer.time = dateTimeInfo[1].split(' Uhr')[0];
-
-    List<String> otherInfo = dateTimeInfo[1].split(' | ');
-    if (otherInfo.isNotEmpty) {
-      clubMatchInfoTransfer.ageGroup = otherInfo[1].trim();
-      clubMatchInfoTransfer.competition = otherInfo[2].trim();
-    }
   }
 }
-
-
