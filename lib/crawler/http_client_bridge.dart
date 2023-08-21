bd41f36b964cd8c2ac3132f36c3f9c655aafde98
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2023-08-21 11:41:22 +0200

    perfomance

--- lib/crawler/http_client_bridge.dart
+++ lib/crawler/http_client_bridge.dart
@@ -8,12 +8,12 @@ abstract interface class HttpClientBridgeInterface {
 class HttpClientBridge implements HttpClientBridgeInterface {
   @override
   Future<String> fetchData(String url) async {
-    var response = await http.get(Uri.parse('https://fussball.de$url'));
+    var response = await http.get(Uri.parse('https://www.fussball.de$url'));
 
     if (response.body != '') {
       return response.body;
     }
 
-    throw Exception('Error on URL: https://fussball.de$url');
+    throw Exception('Error on URL: https://www.fussball.de$url');
   }
 }
