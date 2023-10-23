b7371e3cfcd16ecc28fb0761333c6b4ecd708367
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2023-10-23 14:23:58 +0200

    add execute add new file in gitignore

--- bin/dump.dart
+++ bin/dump.dart
@@ -1,11 +1,8 @@
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:args/args.dart';
+import 'package:api_fussball_dart/database.dart';
 
 const fileName = 'line-number';
 const option = 'option';
 
 void main(List<String> arguments) {
-
+  importUsersFromJson();
 }
